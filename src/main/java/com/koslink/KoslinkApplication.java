ba466c167dc58fc2a1b77e494198fb754867f969
김세훈 <shggm2000@gmail.com>
2026-07-20 20:22:00 +0900

    build: 기본 프로젝트 초기화 및 Gradle 설정 추가

---
+++ src/main/java/com/koslink/KoslinkApplication.java
@@ -0,0 +1,13 @@
+package com.koslink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoslinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoslinkApplication.class, args);
+	}
+
+}
